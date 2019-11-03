d4cba37fb02bc9918c45e8eda4d64996dc39e30f
Faiq <57317125+ifaiq19@users.noreply.github.com>
2019-11-03 20:15:49 +0500

    Add files via upload

---
+++ first assignment.py
@@ -0,0 +1,26 @@
+#Twinkle Twinkle
+print('Twinke, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are!')
+
+#Version
+import sys
+print(sys.version)
+
+#Date & Time
+import datetime
+print(datetime.datetime.now())
+
+#Radius and Area
+radius = int(input('Enter Radius: '))
+Area = (22/7)radius*2
+print(Area)
+
+#Inverted Name
+x = input('Enter First Name: ')
+y = input('Enter Last Name: ')
+print(y, ' ', x)
+
+#Sum of two inputs
+x = int(input('Enter first number: '))
+y = int(input('Enter second number: '))
+total = x + y
+print('Total= ', total)
